7a11a24d389b31c45ab22230312308ceb6e6d8c3
edsay12 <edvandearaujo2@hotmail.com>
2021-07-08 10:46:38 -0300

    Estruturando o protifolio

---
+++ revisao.js
@@ -0,0 +1,11 @@
+// // funçao de alta ordem 
+const produtos = [
+    
+   {id:1,nome:'coca cola',valor:2.00,categoria:'limpesa'},
+    {id:2,nome:'pao',valor:1.00,categoria:'comida'},
+    {id:3,nome:'queijo',valor:2.00,categoria:'comida'},
+    {id:4,nome:'leite',valor:5.00,categoria:'comida'},
+    {id:5,nome:'amaciente',valor:5.56,categoria:'limpesa'}
+]
+
+// reduce e tipo um groud by que retorna somente um valor
